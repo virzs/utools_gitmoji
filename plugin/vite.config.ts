e5d33e8e434b4ad2c90d8561779a0c8818592a27
vir <virs98@outlook.com>
2023-02-19 14:21:57 +0800

    :arrow_up: chore 升级 font awesome

--- plugin/vite.config.ts
+++ plugin/vite.config.ts
@@ -1,20 +1,10 @@
 import { defineConfig } from "vite";
 import preact from "@preact/preset-vite";
-import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
-import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "./",
-  plugins: [
-    preact(),
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), "src/static")],
-      symbolId: "icon-[dir]-[name]",
-      inject: "body-last",
-      customDomId: "__svg__icons__dom__",
-    }),
-  ],
+  plugins: [preact()],
   server: {
     host: "0.0.0.0",
     port: 8101,
