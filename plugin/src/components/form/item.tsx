9bbbc39c28c8c1b8aff1317330819a0fd8be8ba7
vir <virs98@outlook.com>
2023-07-03 10:17:18 +0800

    :children_crossing: chore 优化设置页面样式

--- plugin/src/components/form/item.tsx
+++ plugin/src/components/form/item.tsx
@@ -17,7 +17,7 @@ const FormItem: FC<FormItemProps> = (props) => {
   const { label, description, children } = props;
 
   return (
-    <div className="flex justify-between items-center mb-2 border-b pb-2 border-b-[#e7eaed] dark:border-b-[#2b2c2d] select-none">
+    <div className="flex justify-between items-center mb-2 border-b pb-2 border-b-[#e7eaed] dark:border-b-[#2b2c2d] select-none last:border-b-0 last:mb-0 last:pb-0">
       <div className="text-black dark:text-white">
         <p className="text-sm">{label}</p>
         <p class="text-xs text-gray-800 dark:text-gray-400">{description}</p>
